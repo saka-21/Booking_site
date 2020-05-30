87d214831d59d48dab537f5a2b7f9fdd75e9a9b2
saka-21 <kasaikasai572@gmail.com>
2020-05-30 16:47:55 +0900

    Add:カレンダー機能の追加

--- booking/urls.py
+++ booking/urls.py
@@ -7,4 +7,8 @@ app_name = 'booking'
 urlpatterns = [
     path('', views.StoreList.as_view(), name='store_list'),
     path('store/<int:pk>/staffs/', views.StaffList.as_view(), name='staff_list'),
+    path('staff/<int:pk>/calendar/', views.StaffCalendar.as_view(),
+         name='calendar'),
+    path('staff/<int:pk>/calendar/<int:year>/<int:month>/<int:day>',
+         views.StaffCalendar.as_view(), name='calendar')
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,6 +1,10 @@
+import datetime
+from django.conf import settings
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 from django.views import generic
-from .models import Store, Staff
+from .models import Store, Staff, Schedule
 
 # Create your views here.
 
@@ -23,5 +27,60 @@ class StaffList(generic.ListView):
         store = self.store = get_object_or_404(Store, pk=self.kwargs['pk'])
         queryset = super().get_queryset().filter(store=store)
         return queryset
+
+
+class StaffCalendar(generic.TemplateView):
+    template_name = 'booking/calendar.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        staff = get_object_or_404(Staff, pk=self.kwargs['pk'])
+        today = datetime.date.today()
+
+        # どの日を基準にカレンダーを表示するかの処理
+        # 年月日の指定があればそれを、なければ今日からの表示
+        year = self.kwargs.get('year')
+        month = self.kwargs.get('month')
+        day = self.kwargs.get('day')
+        if year and month and day:
+            base_date = datetime.date(year=year, month=month, day=day)
+        else:
+            base_date = today
+
+        # カレンダーは1週間分表示するので、基準日から1週間の日付を作成しておく
+        days = [base_date + datetime.timedelta(days=day) for day in range(7)]
+        start_day = days[0]
+        end_day = days[-1]
+
+        # 9時から17時まで1時間刻み、1週間分の、値がTrueなカレンダーを作る
+        calendar = {}
+        for hour in range(9, 18):
+            row = {}
+            for day in days:
+                row[day] = True
+            calendar[hour] = row
+
+        # カレンダー表示する最初と最後の日時の間にある予約を取得する
+        start_time = datetime.datetime.combine(start_day, datetime.time(hour=9, minute=0, second=0))
+        end_time = datetime.datetime.combine(end_day, datetime.time(hour=9, minute=0, second=0))
+        for schedule in Schedule.objects.filter(staff=staff).exclude(Q(start__gt=end_time)|Q(end__lt=start_time)):
+            local_dt = timezone.localtime(schedule.start)
+            booking_date = local_dt.date()
+            booking_hour = local_dt.hour
+            if booking_hour in calendar and booking_date in calendar[booking_hour]:
+                calendar[booking_hour][booking_date] = False
+
+        context['staff'] = staff
+        context['calendar'] = calendar
+        context['days'] = days
+        context['start_day'] = start_day
+        context['end_day'] = end_day
+        context['before'] = days[0] - datetime.timedelta(days=7)
+        context['next'] = days[-1] + datetime.timedelta(days=1)
+        context['today'] = today
+        context['public_holidays'] = settings.PUBLIC_HOLIDAYS
+        return context
+
+
 
 
